262418ef93f3f5fcbe9877b1c2d4b6cfb4c4bd9e
sreemanta das <dassreemanta15august@gmail.com>
2025-02-20 17:57:00 +0530

    resource deleted

--- rg.tf
+++ rg.tf
@@ -1,23 +1,23 @@
-resource "azurerm_resource_group" "example" {
-    name = "rg200"
-    location = "eastus"
+# resource "azurerm_resource_group" "example" {
+#     name = "rg200"
+#     location = "eastus"
   
-}
+# }
 
-resource "azurerm_resource_group" "example1" {
-    name = "rg300"
-    location = "eastus"
+# resource "azurerm_resource_group" "example1" {
+#     name = "rg300"
+#     location = "eastus"
   
-}
+# }
 
-resource "azurerm_resource_group" "example2" {
-    name = "rg400"
-    location = "eastus"
+# resource "azurerm_resource_group" "example2" {
+#     name = "rg400"
+#     location = "eastus"
   
-}
+# }
 
-resource "azurerm_resource_group" "example3" {
-    name = "rg500"
-    location = "eastus"
+# resource "azurerm_resource_group" "example3" {
+#     name = "rg500"
+#     location = "eastus"
   
-}
+# }
